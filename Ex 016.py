66e75fd0756902aec63eaed8f0004eaba5d9f6d7
Ewerton082 <Tec.-.Ewerton@hotmail.com>
2024-12-12 17:47:48 -0300

    Alterando o retorno do 16

--- Ex 016.py
+++ Ex 016.py
@@ -2,4 +2,4 @@
 from math import trunc
 
 valor_quebrado = float(input("Me diga um numero que irei tirar os numeros flutuantes : ").strip().replace(',', '.'))
-print(f"{trunc(valor_quebrado)}")
+print(f"O valor que foi passado é de {valor_quebrado} | Sua porção inteira é de: {trunc(valor_quebrado)}".replace('.', ','))
